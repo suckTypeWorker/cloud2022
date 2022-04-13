00062ba8e83410230e80750e679c50076a831135
suckTypeWorker <zy15243299561@outlook.com>
2022-04-13 22:37:03 +0800

    cloud2022 commit Cloud-Config-Client-3355

---
+++ cloud-config-client-3355/src/main/java/com/atguigu/springcloud/controller/ConfigClientController.java
@@ -0,0 +1,21 @@
+package com.atguigu.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author ZY152
+ * @create 2022/4/13 22:31
+ * @description
+ */
+@RestController
+public class ConfigClientController {
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/configInfo")
+    public String getConfigInfo(){
+        return configInfo;
+    }
+}
